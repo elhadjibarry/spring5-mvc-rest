580e48601889f72448df6eaa032a43a7d5356b9c
Elhadji Barry <elhadji.barry@gmail.com>
2019-07-25 07:46:18 -0400

    24.382 RESTFul web services with Spring MVC - Mapstruct

---
+++ src/test/java/guru/springfamework/api/v1/mapper/CategoryMapperTest.java
@@ -0,0 +1,26 @@
+package guru.springfamework.api.v1.mapper;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryMapperTest {
+
+    public static final String NAME = "Fruits";
+    CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+
+    @Test
+    public void categoryToCategoryDTO() {
+
+        Category category = new Category();
+        category.setName(NAME);
+        category.setId(1L);
+
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+
+        assertEquals(Long.valueOf(1L), categoryDTO.getId());
+        assertEquals(NAME, categoryDTO.getName());
+    }
+}
